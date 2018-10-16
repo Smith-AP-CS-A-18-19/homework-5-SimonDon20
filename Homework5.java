f4c892f08b8012f263b02fe73dd385f7075dc2e1
Donovan <SimonDon@berkeleyprep.org>
2018-10-15 23:25:20 -0400

    totally didn't just do this

--- Homework5.java
+++ Homework5.java
@@ -1,10 +1,17 @@
+//Donovan Simonton
+
 public class Homework5 {
 
 	/* Finish the constructor and create any necessary instance
 	 * variables
 	 */
+	 private String str;
+	 private int five;
+	 private int six;
 	public Homework5(String s, int a, int b) {
-
+		str = s;
+		five = a;
+		six = b;
 	}
 
 	/* Return a substring of the stored String, starting at index with
@@ -16,7 +23,11 @@ public class Homework5 {
 	 * substring method in the String class
 	 */
 	public String substring(int index, int length) {
-
+ 			if (index + length < str.length()) {
+				return str.substring(index, index + length);
+			} else {
+				return str.substring(index, str.length());
+			}
 	}
 
 	/* Return true if the stored String contains the provided String,
@@ -25,13 +36,17 @@ public class Homework5 {
 	 * an example
 	 */
 	public boolean contains(String s) {
-		
+		return str.contains(s);
 	}
 
 	/* Return the larger of the two stored numbers
 	 */
-	public static int max() {
-
+	public int max() {
+		if (five > six) {
+			return five;
+		} else {
+			return six;
+		}
 	}
 
 	public static void main(String[] args) {
